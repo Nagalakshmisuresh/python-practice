3ee38fcbd5bfda0d6687f17ca67b7fddfe500fd9
Nagalakshmisuresh <93209339+Nagalakshmisuresh@users.noreply.github.com>
2022-01-04 10:11:13 +0530

    Add files via upload

---
+++ Practise1.py
@@ -0,0 +1,45 @@
+1.Write a program to read a number from the user and print its equivalent
+Binary, Octal, and Hexadecimal values
+Input:
+Enter the Number: 100
+Output:
+Binary Equivalent of 100 is 0b1100100
+Octal Equivalent of 100 is 0o144
+Hexadecimal Equivalent of 100 is 0x64
+--------------------------------------------------------
+2.Write a Python program to create 3 variable and assign them Binary,
+Octal and Hexadecimal values and print the result in their decimal equivalent
+value.
+3.Write a Python program to print the current date and time
+--------------------------------------------------------------------------
+4.Write a Python program to print the current date and time
+--------------------------------------------------------------------------
+5.: Write a Python program to print the ASCII Character for the given
+integer value
+Input:
+Enter a Number: 35
+Output:
+ASCII equivalent of 35 is #
+Input:
+Enter a Number: 125
+Output:
+ASCII equivalent of 125 is }
+Input:
+Enter a Number: 50
+Output:
+ASCII equivalent of 50 is 2
+
+6.Write a Python program to print the following pattern
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+-------------------
+
+
+
+
+
+
+
